4f31ef289f2df1825680b23a44f925329344a720
Imandra <shalyapin.alexej@yandex.ru>
2016-11-19 16:48:38 +0400

    add 1st frontend files

---
+++ ajaxRequest/script.js
@@ -0,0 +1,46 @@
+$.fn.serializeObject = function() {
+	var o = {};
+	var a = this.serializeArray(); // сериализируем данные
+	$.each(a, function() {
+		if (o[this.name]) {
+			if (!o[this.name].push) {
+				o[this.name] = [o[this.name]];
+			}
+			o[this.name].push(this.value || '');
+		} else {
+			o[this.name] = this.value || '';
+		}
+	});
+	return o;
+};
+
+$(function(){
+    $('#my_form').on('submit', function(event){
+        event.preventDefault();//предотвращаем выполнение действия по умолчанию
+        var frm = $(this); //сохраняем ссылку на форму           
+	    var fData = JSON.stringify(frm.serializeObject());
+		  
+        $.ajax({
+            url: frm.attr('action'), // путь к обработчику - берем из атрибута action
+            type: frm.attr('method'), // метод передачи - берем из атрибута method
+            data: {form_data: fData},
+            dataType: 'json',
+            success: function(data){
+                if(data){
+                    //form.replaceWith(data); // заменим форму данными, полученными в ответе.					 
+					 var obj = $.parseJSON(data);	//делаем возвращенную сервером строку объектом и 
+													//обращаемся к нему ниже 
+													//метод depricated, рекомендуется нативный
+					 $('#result').html(data +'<br>'+ obj.login);//вывод ответа в div
+                }
+				//if (obj.login == 'Alex'){
+				//location = 'test.html';//переход по ссылке на другую страницу,				                      
+				//}					  //если логин Alex (будет другая проверка)				
+			},
+			error:  function(xhr, str){
+                     $('#result').html('Критическая ошибка'); 
+			}
+        });
+    });
+});
+
